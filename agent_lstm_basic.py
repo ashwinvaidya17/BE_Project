a0333ea035bfc4209b7225e0d7b4b7bf05ae3a77
Aniket Sanap <aniket.s2509@gmail.com>
2019-01-25 11:23:58 +0530

    Initial commit

---
+++ agent_lstm_basic.py
@@ -0,0 +1,169 @@
+from grabscreen import grab_screen
+import tensorflow as tf
+from tensorflow import keras as keras
+from directKeys import PressKey, ReleaseKey, SPACE
+import cv2
+import numpy as np
+import random
+import time
+from collections import deque
+from flappybirdTry import FlappyBird
+import pygame
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser(description='RL Flappy bird')
+parser.add_argument('-d','--display', help='Display frames', required=False, default='False')
+parser.add_argument('-v','--verbose', help='Description for bar argument', required=False, default='False')
+args = vars(parser.parse_args())
+
+class Agent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.episodes = 1000
+        self.framesPerEpisode = 10000
+        self.model = self.createModel()
+        self.epsilon = 0.1
+        self.minEpsilon = 0.1
+        self.epsilonDecayRate = 0.01
+        self.episodeMoves = deque(maxlen=10000)
+        if args['display'] == 'True':
+            self.display = True
+        else:
+            self.display = False
+        if args['verbose'] == 'True':
+            self.verbose = True
+        else:
+            self.verbose = False
+
+
+    def createModel(self):
+        if os.path.isfile('agent_lstm_basic_model.h5py'):
+            model = keras.models.load_model('agent_lstm_basic_model.h5py')
+        else:
+            model = keras.models.Sequential()
+            # model.add(keras.layers.Flatten(input_shape=(40, 70)))
+
+            model.add(keras.layers.CuDNNLSTM(256, input_shape=(1, 2800), return_sequences=True))
+            model.add(keras.layers.Dropout(0.2))
+
+            model.add(keras.layers.CuDNNLSTM(128, input_shape=(1, 2800), return_sequences=True))
+            model.add(keras.layers.Dropout(0.2))
+
+            model.add(keras.layers.Dense(32, activation='relu'))
+            model.add(keras.layers.Dropout(0.2))
+
+            model.add(keras.layers.Dense(32, activation='relu'))
+            model.add(keras.layers.Dropout(0.2))
+
+            model.add(keras.layers.Dense(2, activation='linear'))
+
+            model.compile(loss='mse', optimizer=keras.optimizers.Adam())
+
+        return model
+
+    def preprocess(self, screen):
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        screen = cv2.resize(screen, (40, 70))
+        screen = screen.reshape(1, 1, 2800)
+        return screen
+
+    def act(self, frame, game):
+        if np.random.rand() < self.epsilon:
+            action = random.randrange(2)
+        else:
+            action = np.argmax(self.model.predict(frame))
+
+        if action == 1:
+            # Jump
+            game.jumpAction()
+            # keyDown('space')
+            # PressKey(SPACE)
+            # time.sleep(0.05)
+            # ReleaseKey(SPACE)
+        return action
+
+    def play(self):
+        try:
+            pygame.font.init()
+            font = pygame.font.SysFont("Arial", 50)
+            for episode in range(1, self.episodes):
+                game = FlappyBird(self.display)
+                clock = pygame.time.Clock()
+                frameCount = 0
+                rewardPerFrame = 1
+                penalty = -100
+                isAlive, screen = game.step(clock, font)
+                screen = self.preprocess(screen)
+                # screen = grab_screen(region=(0, 30, 400, 738))
+                frameCount = frameCount + 1
+
+                while True:
+                    if frameCount == self.framesPerEpisode or isAlive == False:
+                        break
+                    
+                    action = self.act(screen, game)
+                    initialScore = game.counter
+                    isAlive, nextScreen = game.step(clock, font)
+                    # ReleaseKey(SPACE)
+                    # To skip frames
+                    # if action == 1:
+                    #     for i in range(0, 3):
+                    #         isAlive = game.step(clock, font)
+
+                    # nextScreen = grab_screen(region=(0, 30, 400, 738))
+                    nextScreen = self.preprocess(nextScreen)
+                    finalScore = game.counter
+                    if isAlive:
+                        reward = rewardPerFrame
+                        if finalScore > initialScore:
+                            reward = 100
+                    else:
+                        reward = penalty
+                    screen = nextScreen
+                    self.episodeMoves.append([screen, nextScreen, reward, isAlive, action])
+                    if self.verbose:
+                        print("Action: {}, Epsilon: {}, reward: {}".format(action, self.epsilon, reward))
+                    frameCount = frameCount + 1
+
+
+                print("Episode #{}, Score: {}, Frames: {}".format(episode, finalScore, frameCount))
+                self.train()
+                if episode % 40 == 0:
+                    self.model.save('agent_lstm_basic_model.h5py')
+
+        except KeyboardInterrupt:
+            self.model.save('agent_lstm_basic_model.h5py')
+
+
+    def train(self):
+        batchSize = 1000
+        if batchSize > len(self.episodeMoves):
+            batchSize = len(self.episodeMoves)
+        
+        minibatch = random.sample(self.episodeMoves, batchSize)
+        for batch in minibatch:
+            state = batch[0]
+            nextState = batch[1]
+            reward = batch[2]
+            isDone = batch[3]
+            action = batch[4]
+
+            target = reward     # in case of penalty
+            if not isDone:
+                target = reward + np.amax(self.model.predict(nextState)[0])
+
+            target_f = self.model.predict(state)
+            target_f[0][0][action] = target
+
+            # print("target: {}\nlen(target_f): {}\ntarget_f: {}".format(target, len(target_f), target_f))
+
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+
+        if self.epsilon > self.minEpsilon:
+            self.epsilon = self.epsilon - self.epsilonDecayRate
+
+
+if __name__ == '__main__':
+    Agent().play()
---
+++ flappybirdTry.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import pygame
+from pygame.locals import *  # noqa
+import sys
+import random
+import numpy as np
+import os
+
+class FlappyBird:
+    def __init__(self, display):
+        if display:
+            self.screen = pygame.display.set_mode((400, 708))
+        else:
+            os.environ['SDL_VIDEODRIVER'] = 'dummy'
+            self.screen = pygame.display.set_mode((1, 1))
+        self.bird = pygame.Rect(65, 50, 50, 50)
+        self.background = pygame.image.load("assets/background.png").convert()
+        self.birdSprites = [pygame.image.load("assets/1.png").convert_alpha(),
+                            pygame.image.load("assets/2.png").convert_alpha(),
+                            pygame.image.load("assets/dead.png")]
+        self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
+        self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
+        self.gap = 200
+        self.wallx = 400
+        self.birdY = 350
+        self.jump = 0
+        self.jumpSpeed = 10
+        self.gravity = 5
+        self.dead = False
+        self.sprite = 0
+        self.counter = 0
+        self.offset = random.randint(-110, 110)
+        self.updateCounter = True
+        self.display = display
+
+    def updateWalls(self):
+        self.wallx -= 2
+        if self.wallx < -80:
+            self.wallx = 400
+            self.offset = random.randint(-110, 110)
+            self.updateCounter = True
+
+        if self.wallx < 40 and self.updateCounter:
+            self.counter += 1
+            self.updateCounter = False
+
+    def birdUpdate(self):
+        if self.jump:
+            self.jumpSpeed -= 1
+            self.birdY -= self.jumpSpeed
+            self.jump -= 1
+        else:
+            self.birdY += self.gravity
+            self.gravity += 0.2
+        self.bird[1] = self.birdY
+        upRect = pygame.Rect(self.wallx,
+                             360 + self.gap - self.offset + 10,
+                             self.wallUp.get_width() - 10,
+                             self.wallUp.get_height())
+        downRect = pygame.Rect(self.wallx,
+                               0 - self.gap - self.offset - 10,
+                               self.wallDown.get_width() - 10,
+                               self.wallDown.get_height())
+        if upRect.colliderect(self.bird):
+            self.dead = True
+        if downRect.colliderect(self.bird):
+            self.dead = True
+        # The changes    
+        if self.bird[1] > 708:
+            self.dead = True
+        elif self.bird[1] < 10:
+            self.dead = True
+        if not 0 < self.bird[1] < 720:
+            self.bird[1] = 50
+            self.birdY = 50
+            self.dead = False
+            self.counter = 0
+            self.wallx = 400
+            self.offset = random.randint(-110, 110)
+            self.gravity = 5
+
+    def jumpAction(self):
+        self.jump = 17
+        self.gravity = 5
+        self.jumpSpeed = 10
+
+    def step(self, clock, font):
+        clock.tick(60)
+        # for event in pygame.event.get():
+        #     if event.type == pygame.QUIT:
+        #         sys.exit()
+        #     if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
+        #         self.jump = 17
+        #         self.gravity = 5
+        #         self.jumpSpeed = 10
+
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.wallUp,
+                            (self.wallx, 360 + self.gap - self.offset))
+        self.screen.blit(self.wallDown,
+                            (self.wallx, 0 - self.gap - self.offset))
+        self.screen.blit(font.render(str(self.counter),
+                                        -1,
+                                        (255, 255, 255)),
+                            (200, 50))
+        if self.dead:
+            self.sprite = 2
+            return False, pygame.surfarray.array3d(pygame.display.get_surface())
+        elif self.jump:
+            self.sprite = 1
+        self.screen.blit(self.birdSprites[self.sprite], (70, self.birdY))
+        if not self.dead:
+            self.sprite = 0
+        self.updateWalls()
+        self.birdUpdate()
+        if self.display:
+            pygame.display.update()
+        return True, pygame.surfarray.array3d(pygame.display.get_surface())
+
+
+if __name__ == '__main__':
+    
+    pygame.font.init()
+    font = pygame.font.SysFont("Arial", 50)
+    clock = pygame.time.Clock()
+    while FlappyBird(True).step(clock, font):
+        pass
